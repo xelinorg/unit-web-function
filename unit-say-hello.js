fda14abb7c59833b35671dbbc88a5193f020f4e0
@xelin.org <alex@systemics.gr>
2021-05-05 03:37:16 +0300

    Update unit-say-hello.js

--- unit-say-hello.js
+++ unit-say-hello.js
@@ -1,4 +1,4 @@
-function (req, res) {
+module.expors = function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Hello, Node.js on Unit from the distributed function!")
 }
